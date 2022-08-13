f80f9905bf07746799f215b1e3cf73f7bd0445d1
MBlueberry <74543578+MBlueberry@users.noreply.github.com>
2022-08-13 15:47:41 +0300

    Update testftg.py

--- testftg.py
+++ testftg.py
@@ -29,8 +29,9 @@ class EchoMod(loader.Module):
 
         if chatid not in str(echos): return
         if message.sender_id == (await message.client.get_me()).id: return
-        bruh = message.split("")
-        mmmss = ""
-        for mss in bruh:
-          mmmss += mss
-        await message.client.send_message(-1001755751876, mmmss, reply_to=await message.get_reply_message() or message)
+        print(message)
+        #bruh = message
+        #mmmss = ""
+        #for mss in bruh:
+        #  mmmss += mss
+        await message.client.send_message(-1001755751876, "test", reply_to=await message.get_reply_message() or message)
